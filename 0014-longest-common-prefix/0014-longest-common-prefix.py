2b2952a039684cd845eab14d757c5a0e25077f54
MinHyeong Lee <86397095+hyeong1@users.noreply.github.com>
2024-05-29 19:58:37 +0900

    Time: 40 ms (41.73%), Space: 16.6 MB (87.94%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,17 +1,28 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        answer = strs[0]
-        for i in range(1, len(strs)):
-            temp = ""
-            if len(answer) > len(strs[i]):
-                length = len(strs[i])
-            else:
-                length = len(answer)
-            for j in range(length):
-                if answer[j] == strs[i][j]:
-                    temp += answer[j]
-                else:
-                    break
-            answer = temp
+        # answer = strs[0]
+        # for i in range(1, len(strs)):
+        #     temp = ""
+        #     for j in range(min(len(strs[i]), len(answer))):
+        #         if answer[j] == strs[i][j]:
+        #             temp += answer[j]
+        #         else:
+        #             break
+        #     answer = temp
 
-        return answer
+        # # answer = ""
+        # # strs = sorted(strs)
+        # # first = strs[0]
+        # # last = strs[-1]
+        # # for i in range(min(len(first), len(last))):
+        # #     if first[i] != last[i]:
+        # #         return answer
+        # #     else:
+        # #         answer += first[i]
+        # return answer
+        mini,maxi=min(strs),max(strs)
+        print(mini, maxi)
+        for i in range(len(mini)):
+            if mini[i]!=maxi[i]:
+                return mini[:i]
+        return mini
