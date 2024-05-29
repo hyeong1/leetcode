685ac7bac3696c0f2d12c31ec7ca97cef8d870ae
MinHyeong Lee <86397095+hyeong1@users.noreply.github.com>
2024-05-29 17:00:33 +0900

    Time: 49 ms (15.77%), Space: 17.6 MB (95.02%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.py
+++ 0412-fizz-buzz/0412-fizz-buzz.py
@@ -1,23 +1,16 @@
 class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
         string_list = []
-        # for i in range(1, n+1):
-        #     if i % 3 == 0 and i % 5 == 0:
-        #         string_list.append("FizzBuzz")
-        #     elif i % 3 == 0:
-        #         string_list.append("Fizz")
-        #     elif i % 5 == 0:
-        #         string_list.append("Buzz")
-        #     else:
-        #         string_list.append(str(i))
-
         for i in range(1, n + 1):
             string_list.append(str(i))
-        for i in range(2, n, 3):
-            string_list[i] = "Fizz"
-        for i in range(4, n, 5):
-            string_list[i] = "Buzz"
-        for i in range(14, n, 15):
-            string_list[i] = "FizzBuzz"
+
+        for i in range(1, n+1):
+            entry = ""
+            if i % 3 == 0:
+                entry += "Fizz"
+            if i % 5 == 0:
+                entry += "Buzz"
+            if i % 3 == 0 or i % 5 == 0:
+                string_list[i-1] = entry
 
         return string_list
